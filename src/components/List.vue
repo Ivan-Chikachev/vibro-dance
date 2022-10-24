8d54d86a4035560ce3a66f5c616ae6caf0f1b764
Ivan Chikachev <ich@fivegen.ru>
2022-10-24 23:18:25 +0300

    add vibrate dance

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="list">
-    <button class="btn" @click="clickOk">Вибро денс</button>
+    <button class="btn" @click="click">Вибро клик</button>
+    <button class="btn" @click="clickDance">Вибро денс</button>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    clickOk() {
+    click() {
       window.navigator.vibrate([200]);
+    },
+    clickDance() {
+      window.navigator.vibrate([50, 10, 100, 100, 50,50, 400,400,10,10,10,10,5,5,5,5,1000]);
     }
   }
 };
