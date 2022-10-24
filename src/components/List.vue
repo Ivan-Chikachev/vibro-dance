d99ece59cca93e0c473b943e16bdcc46341e6da6
Ivan Chikachev <ich@fivegen.ru>
2022-10-24 23:06:33 +0300

    first commit

---
+++ src/components/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="list">
+    <button class="btn" @click="clickOk">Вибро денс</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clickOk() {
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+html, body, #app {
+  height: 100%;
+}
+.list {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.btn {
+  padding: 20px 40px;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo">
+
+  </div>
+</template>
+
+<script setup>
+
+const a = ['asdas', 'asdasd']
+
+fn()
+
+function fn(acc, item) {
+
+  return acc + item
+}
+
+console.log(a.reduce(fn, 10));
+
+
+
+</script>
